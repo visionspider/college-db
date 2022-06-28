f40362291588e252084ad71c575675532b058552
David Guillaumant-Mergler <47602483+visionspider@users.noreply.github.com>
2022-06-28 17:15:41 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
  define ('HOST', 'localhost');
- define ('USER', 'david');
- define ('PASS', '75sWV8ZFX');
- define ('DB', 'david')
+ define ('USER', 'Your username');
+ define ('PASS', 'Your password');
+ define ('DB', 'Your username')
 ?>
